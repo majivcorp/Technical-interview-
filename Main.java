ddf5fce80d26a3355aab4f7b307b271c6842e27b
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-03-16 21:51:49 +0000

    pushing to github

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.*;
-import java.util.*;
+import java.util.*; 
 
 class Main {
   public static void main(String[] args) {
