bdba68c979372144ec83ee66c4ba98dcad94de06
Tobi Oriola <44350099+majivcorp@users.noreply.github.com>
2022-03-16 21:28:48 +0000

    comments explaining code

--- Main.java
+++ Main.java
@@ -10,47 +10,55 @@ class Main {
 
     //declaration and intialization
     
-    //int[] nArray = new int[9]; 
+    int[] nArray = new int[9]; 
     
-    int[] nArray = { 1, 3, 0, 6, 9, 2, 5, 4, 4};
+    //int[] nArray = { 1, 3, 0, 6, 9, 2, 5, 4, 4}; // intial test case
     int[] zArray = new int[9];
     int[] yArray = { 1, 2, 1, 2, 1, 2, 1, 2, 1};
     final int size = 9;
     int num = 0;
     int sum = 0;
-    int[] copArray = new int[9];
     
     // For reading  the element
-    /*   < - commentted out for test case
     for (int i = 0; i < size; i++) {
       nArray[i] = input.nextInt();
     }
-    */
+    
 
-    // For adding arrays
+    // For loop adds the two arrays by transversing through the loop.
     
     for (int i = 0; i < size; i++) {
-      int s = 0, digit, n = 0;
+      int s = 0, digit = 0, n = 0;
 
+      // if the sum we get is a 2 digit we add the digits together
       if (nArray[i] * yArray[i] > 9) {
-        n = nArray[i] * yArray[i];
-  
+        zArray[i] = nArray[i] * yArray[i];
+        
+        n = zArray[i];
+        //the while loop iterates till the sum of the digit is derived
         while(n != 0) {
           digit = n % 10;
           s = s + digit;
           n = n/10;
         }
         
-        zArray[i] = n;
+        zArray[i] = s;
+      
+        System.out.println(s); //print out test, 
+        //to test if we are getting the correct digit from the while loop above
 
       } else {
-        zArray[i] = nArray[i] * yArray[i];
+        zArray[i] = nArray[i] * yArray[i]; 
       }
     
     }
 
+    /* Method to test the validitiy of thee sin */
     num = nArray[0];
 
+    //The first digit of a SIN is 8 or 0, which indicates 
+    //that it is not a canadaian province of registration.
+    //The program will terminate after printing the "SIN not Valid"
     switch(num) {
         //Case statements
       case 8: System.out.println("SIN not Valid");
@@ -59,10 +67,11 @@ class Main {
       case 0: System.out.println("SIN not Valid");
         System.exit(0); //to Terminate a Java Program
         break;
+      case 0: System.out.println("Immigrants & other temp SIN's");
+        break;
     }
-    
-    //sum of elements in array
-  
+
+    /* Method to sum of elements in array*/
     for (int i : zArray)
       sum += i;
     // print test
@@ -70,9 +79,7 @@ class Main {
     System.out.println(zArray[3]);
     
     
-    
-    //SIN verification if sum % 10 == 0
-
+    /* Method for SIN verification if sum % 10 == 0 */
     if (sum % 10 == 0) {
       System.out.println("True");
     } else {
